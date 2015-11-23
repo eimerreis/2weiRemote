b6c285f9cb6c294cd478a959c175786ba46837f1
eimerreis <imorice95@googlemail.com>
2015-11-23 14:51:10 +0000

    Baisc HTTP Auth hinzugefügt!

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var auth = require('basic-auth-connect');
 var app = express();
 
+
+app.use(auth('eimerreis', '1234'));
 app.use(express.static(__dirname + "/backend"));
 app.listen(process.env.PORT || 3000);
 console.log("Server läuft auf Port 80");
