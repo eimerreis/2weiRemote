f8eb935f3234545c40de304a705df831738be57a
eimerreis <imorice95@googlemail.com>
2015-11-23 15:39:01 +0000

    CORS löst Probleme mit der Ladezeit aus und wurde auskommentiert.

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 var express = require('express');
 var auth = require('basic-auth-connect');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var io = require('socket.io');
 var cors = require('cors');
 
-app.use(cors);
+//app.use(cors);
 
 //Zu Testzwecken wurde die Authentifizerung auskommentiert.
 //app.use(auth('eimerreis', '1234'));
 
 app.use(express.static(__dirname + "/backend"));
 
-  io.on('connection', function(socket){
-    console.log("User Connected");
-  });
 
-server.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000);
+var socket = io.listen(server);
